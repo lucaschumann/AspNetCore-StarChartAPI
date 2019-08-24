a3f6a126dd68d368ee17e1f7bdd8e3f4a6101c84
Luca Schumann <lucaschumann@fbmtl.com>
2019-08-24 09:40:44 +0200

    Step 3 Fix

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StarChart.Models
 {
@@ -17,6 +18,7 @@ namespace StarChart.Models
 
         public int? OrbitedObjectId { get; set; }
 
+        [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
 
         public TimeSpan OrbitalPeriod { get; set; }
